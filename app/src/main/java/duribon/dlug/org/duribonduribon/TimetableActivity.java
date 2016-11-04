52e71000f0707a2335bb20c7833b8ee85057e27a
Neon K.I.D <clax1412@gmail.com>
2016-11-04 19:26:29 +0900

    MapView로 Fragment 변경

--- app/src/main/java/duribon/dlug/org/duribonduribon/TimetableActivity.java
+++ app/src/main/java/duribon/dlug/org/duribonduribon/TimetableActivity.java
@@ -11,6 +11,6 @@ public class TimetableActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView();
+        // setContentView();
     }
 }
--- app/src/main/java/duribon/dlug/org/duribonduribon/fragment/MapFragment.java
+++ app/src/main/java/duribon/dlug/org/duribonduribon/fragment/MapFragment.java
@@ -2,11 +2,18 @@ package duribon.dlug.org.duribonduribon.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.InflateException;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import butterknife.ButterKnife;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import duribon.dlug.org.duribonduribon.R;
 
 /**
@@ -14,19 +21,32 @@ import duribon.dlug.org.duribonduribon.R;
  */
 
 public class MapFragment extends Fragment {
-    public static MapFragment newInstance() {
-        MapFragment fragment = new MapFragment();
-        return fragment;
-    }
+    private GoogleMap mMap;
+    View view;
 
-    public MapFragment() {
+    static final LatLng DKU = new LatLng(36.836609, 127.168095);
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceBundle) {
-        View rootview = inflater.inflate(R.layout.activity_map, container, false);
-        ButterKnife.inject(this, rootview);
-        return rootview;
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        try {
+            view = inflater.inflate(R.layout.activity_map, container, false);
+            // Bundle data = getArguments();
+            // String schoolname = data.getString("Dankook University");
+
+            mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
+
+            mMap.addMarker(new MarkerOptions().position(DKU).title("단국대학교 대운동장"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(DKU, 15));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(17), 2000, null);
+
+        } catch(InflateException ex) {
+
+        }
+        return view;
     }
 }
