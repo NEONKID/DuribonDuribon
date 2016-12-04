cc68927a76d0925e7cb8e0a2a58a0c0e5b9648de
Itblock <wnsuddlsla@gmail.com>
2016-12-04 20:28:33 +0900

    핀치 줌 설정 및 지도

--- app/src/main/java/duribon/dlug/org/duribonduribon/Activities/InteriorMapActivity.java
+++ app/src/main/java/duribon/dlug/org/duribonduribon/Activities/InteriorMapActivity.java
@@ -6,13 +6,18 @@ import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 import duribon.dlug.org.duribonduribon.Fragments.MapFragment;
 import duribon.dlug.org.duribonduribon.R;
-import uk.co.senab.photoview.PhotoViewAttacher;
+import pl.polidea.view.ZoomView;
+import android.view.LayoutInflater;
+import android.content.Context;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 /**
  * Created by neonkid on 11/5/16.
@@ -20,7 +25,6 @@ import uk.co.senab.photoview.PhotoViewAttacher;
  * 내부 지도를 구현한 Activity,,
  */
 public class InteriorMapActivity extends AppCompatActivity {
-    @InjectView(R.id.interior_content)
     CoordinatorLayout coordinatorLayout;    // 내부 지도 레이아웃..
 
     @Override
@@ -31,9 +35,16 @@ public class InteriorMapActivity extends AppCompatActivity {
 
         MapFragment.room_flag = false;  // 원상 복귀,,
 
-        PhotoViewAttacher attacher;
-        ImageView entrance = (ImageView)findViewById(R.id.entrance);
-        attacher = new PhotoViewAttacher(entrance);
+        View v = ((LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE)).inflate(R.layout.activity_interiormap, null, false);
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+
+        ZoomView zoomView = new ZoomView(this);
+        zoomView.addView(v);
+        zoomView.setLayoutParams(layoutParams);
+        zoomView.setMaxZoom(4f); // 줌 Max 배율 설정  1f 로 설정하면 줌 안됩니다
+
+        CoordinatorLayout container = (CoordinatorLayout) findViewById(R.id.dest_content);
+        container.addView(zoomView);
         /*if()
         {
         //검색 값에 따라서 DestinationMap에서 보여주는 층수를 다르게 해야함
@@ -45,15 +56,15 @@ public class InteriorMapActivity extends AppCompatActivity {
         */
     }
 
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
         overridePendingTransition(R.anim.anim_slide_in_left, R.anim.anim_slide_out_right);
     }
 
-    public void nextFloor(View v) {
-        Intent intent = new Intent(this, DestinationMapActivity.class);
-        startActivity(intent);
-        overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+    public void Previousbutton(View v) {
+        finish();
     }
+
 }
